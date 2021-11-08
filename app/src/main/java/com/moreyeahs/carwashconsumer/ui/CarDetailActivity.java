4ec01483047af5bcca4c86e346f28af11ce32932
DESKTOP-GG27BT4\Office <psatpute@moreyeahs.co>
2021-11-08 17:06:47 +0530

    Added car details screen

---
+++ app/src/main/java/com/moreyeahs/carwashconsumer/ui/CarDetailActivity.java
@@ -0,0 +1,16 @@
+package com.moreyeahs.carwashconsumer.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moreyeahs.carwashconsumer.R;
+
+public class CarDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_detail);
+    }
+}
